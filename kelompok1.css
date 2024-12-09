f1ae2ac944aae1f2f3a5293ed9459141c6c57edf
nangningnong <triairsanti@gmail.com>
2024-12-09 12:19:55 +0700

    Add files via upload
    Saya mengunggah file css

---
+++ kelompok1.css
@@ -0,0 +1,21 @@
+<style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin: 20px 0;
+        }
+        table, th, td {
+            border: 1px solid #ddd;
+        }
+        th, td {
+            padding: 10px;
+            text-align: center;
+        }
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+        h1, h2 {
+            text-align: center;
+        }
+    </style>
